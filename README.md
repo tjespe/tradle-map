1eadfd711f1d96ee702b7a478e230dd08b165133
Tord Johan Espe <tordjohanespe@gmail.com>
2024-08-27 15:48:01 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Tradle map
+
+Map of all the countries in the world exactly matching the list of countries used by OEC Tradle.
+
+![Latest map](./build/map.svg)
---
+++ main.py
@@ -0,0 +1,370 @@
+# %%
+import os
+import matplotlib.pyplot as plt
+import geopandas as gpd
+import numpy as np
+import pandas as pd
+from adjustText import adjust_text
+import matplotlib.patheffects as pe
+
+
+# %%
+# List of country names and territories from Tradle
+tradle_countries = [
+    "Afghanistan",
+    "Albania",
+    "Algeria",
+    "American Samoa",
+    "Andorra",
+    "Angola",
+    "Anguilla",
+    "Antigua and Barbuda",
+    "Argentina",
+    "Armenia",
+    "Aruba",
+    "Australia",
+    "Austria",
+    "Azerbaijan",
+    "Bahamas",
+    "Bahrain",
+    "Bangladesh",
+    "Barbados",
+    "Belarus",
+    "Belgium",
+    "Belize",
+    "Benin",
+    "Bermuda",
+    "Bhutan",
+    "Bolivia",
+    "Bonaire",
+    "Bosnia and Herzegovina",
+    "Botswana",
+    "Brazil",
+    "British Indian Ocean Territory",
+    "British Virgin Islands",
+    "Brunei",
+    "Bulgaria",
+    "Burkina Faso",
+    "Myanmar",
+    "Burundi",
+    "Cambodia",
+    "Cameroon",
+    "Canada",
+    "Cape Verde",
+    "Cayman Islands",
+    "Central African Republic",
+    "Chad",
+    "Chile",
+    "China",
+    "Taiwan",
+    "Christmas Island",
+    "Cocos (Keeling) Islands",
+    "Colombia",
+    "Comoros",
+    "Cook Islands",
+    "Costa Rica",
+    "Ivory Coast",
+    "Croatia",
+    "Cuba",
+    "Curacao",
+    "Cyprus",
+    "Czechia",
+    "Democratic Republic of the Congo",
+    "Denmark",
+    "Djibouti",
+    "Dominica",
+    "Dominican Republic",
+    "Ecuador",
+    "Egypt",
+    "El Salvador",
+    "Equatorial Guinea",
+    "Eritrea",
+    "Estonia",
+    "Eswatini",
+    "Ethiopia",
+    "Falkland Islands",
+    "Fiji",
+    "Finland",
+    "France",
+    "French Polynesia",
+    "French Southern Territories",
+    "Gabon",
+    "Gambia",
+    "Georgia",
+    "Germany",
+    "Ghana",
+    "Gibraltar",
+    "Greece",
+    "Greenland",
+    "Grenada",
+    "Guam",
+    "Guatemala",
+    "Guinea",
+    "Guinea-Bissau",
+    "Guyana",
+    "Haiti",
+    "Honduras",
+    "Hong Kong",
+    "Hungary",
+    "Iceland",
+    "India",
+    "Indonesia",
+    "Iran",
+    "Iraq",
+    "Ireland",
+    "Israel",
+    "Italy",
+    "Jamaica",
+    "Japan",
+    "Jordan",
+    "Kazakhstan",
+    "Kenya",
+    "Kiribati",
+    "Kosovo",
+    "Kuwait",
+    "Kyrgyzstan",
+    "Laos",
+    "Latvia",
+    "Lebanon",
+    "Lesotho",
+    "Liberia",
+    "Libya",
+    "Lithuania",
+    "Luxembourg",
+    "Macau",
+    "Madagascar",
+    "Malawi",
+    "Malaysia",
+    "Maldives",
+    "Mali",
+    "Malta",
+    "Marshall Islands",
+    "Mauritania",
+    "Mauritius",
+    "Mexico",
+    "Micronesia",
+    "Moldova",
+    "Mongolia",
+    "Montenegro",
+    "Montserrat",
+    "Morocco",
+    "Mozambique",
+    "Namibia",
+    "Nauru",
+    "Nepal",
+    "Netherlands",
+    "New Caledonia",
+    "New Zealand",
+    "Nicaragua",
+    "Niger",
+    "Nigeria",
+    "Niue",
+    "Norfolk Island",
+    "North Korea",
+    "North Macedonia",
+    "Northern Mariana Islands",
+    "Norway",
+    "Oman",
+    "Pakistan",
+    "Palau",
+    "Palestine",
+    "Panama",
+    "Papua New Guinea",
+    "Paraguay",
+    "Peru",
+    "Philippines",
+    "Pitcairn Islands",
+    "Poland",
+    "Portugal",
+    "Qatar",
+    "Republic of the Congo",
+    "Romania",
+    "Russia",
+    "Rwanda",
+    "Saint Barthelemy",
+    "Saint Helena",
+    "Saint Kitts and Nevis",
+    "Saint Lucia",
+    "Saint Martin",
+    "Saint Pierre and Miquelon",
+    "Saint Vincent and the Grenadines",
+    "Samoa",
+    "San Marino",
+    "Sao Tome and Principe",
+    "Saudi Arabia",
+    "Senegal",
+    "Serbia",
+    "Seychelles",
+    "Sierra Leone",
+    "Singapore",
+    "Slovakia",
+    "Slovenia",
+    "Solomon Islands",
+    "Somalia",
+    "South Africa",
+    "South Korea",
+    "South Sudan",
+    "Spain",
+    "Sri Lanka",
+    "Sudan",
+    "Suriname",
+    "Sweden",
+    "Switzerland",
+    "Syria",
+    "Tajikistan",
+    "Tanzania",
+    "Thailand",
+    "Timor-Leste",
+    "Togo",
+    "Tokelau",
+    "Tonga",
+    "Trinidad and Tobago",
+    "Tunisia",
+    "Turkey",
+    "Turkmenistan",
+    "Turks and Caicos Islands",
+    "Tuvalu",
+    "Uganda",
+    "Ukraine",
+    "United Arab Emirates",
+    "United Kingdom",
+    "United States of America",
+    "Uruguay",
+    "Uzbekistan",
+    "Vanuatu",
+    "Venezuela",
+    "Vietnam",
+    "Wallis and Futuna",
+    "Yemen",
+    "Zambia",
+    "Zimbabwe",
+]
+
+# %%
+# Load world map
+shapefile_dir = "110m_cultural"
+file_dir = os.path.dirname(os.path.realpath(__file__))
+shapefile_dir = os.path.join(file_dir, shapefile_dir)
+shapefile_paths = [
+    "ne_110m_admin_0_countries.shp",
+    "ne_110m_admin_0_tiny_countries.shp",
+    "ne_110m_admin_0_pacific_groupings.shp",
+    "ne_110m_admin_0_sovereignty.shp",
+]
+gdfs = [gpd.read_file(os.path.join(shapefile_dir, path)) for path in shapefile_paths]
+gdf = gpd.GeoDataFrame(pd.concat(gdfs, ignore_index=True))
+gdf
+
+# %%
+# Load ISO 3166-1 country centroids
+centroids = pd.read_json(os.path.join(file_dir, "iso-country-centroids.json"))
+centroids
+
+# %%
+# Map Tradle country names to ISO names
+iso_mapping = {
+    "Democratic Republic of the Congo": "Congo [DRC]",
+    "Ivory Coast": "Côte d'Ivoire",
+    "Myanmar": "Myanmar [Burma]",
+    "Republic of the Congo": "Congo [Republic]",
+    "United States of America": "United States",
+    "Falkland Islands": "Falkland Islands [Islas Malvinas]",
+    "Palestine": "Gaza Strip",  # Assuming Palestine refers to the Gaza Strip
+    "Eswatini": "Swaziland",
+    "Cocos (Keeling) Islands": "Cocos [Keeling] Islands",
+    "Czechia": "Czech Republic",
+    "North Macedonia": "Macedonia [FYROM]",
+    "Sao Tome and Principe": "São Tomé and Príncipe",
+}
+mapped_countries = set(
+    iso_mapping.get(country, country) for country in tradle_countries
+)
+mapped_countries
+
+# %%
+# Check if all countries were found
+found_countries = set(centroids["name"].unique())
+missing_countries = mapped_countries - found_countries
+if missing_countries:
+    print(
+        f"Could not find the following {len(missing_countries)} countries: {missing_countries}"
+    )
+    not_used = found_countries - mapped_countries
+    print(
+        f"The following {len(not_used)} countries exist in ISO but was not used: {not_used}"
+    )
+
+# %%
+# Map some Tradle countries to different labels for aesthetic purposes
+label_mapping = {
+    "Antigua and Barbuda": "Antigua",
+    "Bosnia and Herzegovina": "Bosnia",
+    "Saint Kitts and Nevis": "St. Kitts",
+}
+
+# %%
+# Plot map with labels
+fig, ax = plt.subplots(figsize=(60, 30))
+gdf.plot(ax=ax, color="lightblue", edgecolor=(0, 0, 0, 0.2))
+
+texts = []
+initial_text_positions = []
+
+for tradle_country in tradle_countries:
+    iso_country = iso_mapping.get(tradle_country, tradle_country)
+    row = centroids.loc[centroids["name"] == iso_country]
+    if row.empty:
+        continue
+    lat = float(row["latitude"])
+    lon = float(row["longitude"])
+    country_label = label_mapping.get(tradle_country, tradle_country)
+    text = plt.text(
+        lon,
+        lat,
+        country_label,
+        fontsize=8,
+        ha="center",
+        va="center",
+        path_effects=[pe.withStroke(linewidth=2, foreground=(1, 1, 1, 0.4))],
+    )
+    texts.append(text)
+    initial_text_positions.append((text.get_position()))
+    plt.scatter(lon, lat, color=(1, 0, 0, 0.5), s=5)
+
+
+# Adjust text positions to avoid overlap
+adjust_text(texts, ax=ax, only_move={"text": "xy"})
+
+# Re-check positions and add arrows only if moved significantly
+threshold = 3  # Define the significance threshold
+
+
+# Function to calculate Euclidean distance
+def calculate_distance(pos1, pos2):
+    return np.sqrt((pos1[0] - pos2[0]) ** 2 + (pos1[1] - pos2[1]) ** 2)
+
+
+for i, text in enumerate(texts):
+    current_position = text.get_position()
+    initial_position = initial_text_positions[i]
+    distance_moved = calculate_distance(initial_position, current_position)
+    print(f"{text.get_text()}: {distance_moved}")
+
+    if distance_moved > threshold:
+        text.set_ha("center")
+        text.set_va("center")
+        ax.annotate(
+            "",
+            xy=initial_position,
+            xytext=current_position,
+            arrowprops=dict(arrowstyle="-", color=(1, 0, 0, 0.5)),
+        )
+
+plt.title("Map with Labeled Countries")
+plt.tight_layout(pad=0)
+plt.savefig("build/map.svg")
+plt.savefig("build/map.png")
+plt.show()
+os.system("open build/map.svg")
+
+# %%
